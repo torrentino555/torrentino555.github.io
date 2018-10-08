ec38d8afcc9517c7206ab43e1d3e1612fb3b9070
olof <orl_vlad@mail.ru>
2018-10-08 23:49:06 +0300

    Commit

--- wp-content/themes/ekspro/resource/js/main.js
+++
@@ -1,8 +0,0 @@
-var elem = $('.wrapper');
-var fixViewport = function () {
-    if (elem.css('min-height') != window.innerHeight + "px") {
-        elem.css('min-height', window.innerHeight);
-    }
-};
-
-setInterval(fixViewport, 200);
